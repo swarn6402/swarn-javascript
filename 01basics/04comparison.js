21df9d77c257b8d05cbcd7c96e9af0fbc9e3ef83
Swarnjeet <148362854+swarn6402@users.noreply.github.com>
2024-09-19 07:25:16 +0000

    Comparison notes in JS

---
+++ 01basics/04comparison.js
@@ -0,0 +1,26 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+//console.log("2" > 1);
+//console.log("02" > 1);
+
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+// The reason is that an equality check == and comparisons > < >= <= work differently. 
+// Comparisons convert null to a number, treating it as 0. 
+// That's why (null >= 0) is true whereas, (null > 0) is false.
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === (checks not only the values, but also the datatype. then returns true or false)
+// console.log("2" == 2) returns true, but triple equal returns false.
+
+console.log("2" === 2);
