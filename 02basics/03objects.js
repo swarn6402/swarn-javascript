efd89273bb58267e6e8d8c372203b30e571e22f3
Swarnjeet <148362854+swarn6402@users.noreply.github.com>
2024-09-21 14:19:48 +0000

    Objects part 1 added

---
+++ 02basics/03objects.js
@@ -0,0 +1,35 @@
+// Singleton objects are created with a constructor --> Object.create
+
+// Object Literals:
+const mySymbol = Symbol("key1")
+const JsUser = {
+    name : "Swarn",
+    fullname : "Swarnjeet Nath Tiwary",
+    [mySymbol] : "myKey1",
+    age : 22,
+    location : "patna",
+    email : "swarn@gmail.com",
+    isLoggedin : false,
+    lastLoggedin : ["Monday", "Thursday"]
+}
+
+// console.log(JsUser.email);  --> prints the email, but this isn't the best way to access objects
+console.log(JsUser["email"]);   // better way to access objects. Also, "email" key while declaring objects is treated a string only.
+console.log(JsUser[mySymbol]);  // IMP for interviews --> the way to declare and then access a symbol in an oobject. 
+
+JsUser.email = "swarnjeet@google.com"  // how to change a value in an Object
+// Object.freeze(JsUser) // freezes the object and no changes from here on can be performed on JsUser
+
+
+JsUser.greeting = function(){          // using dot notation to add the greeting method to the JsUser object. This means that now JsUser has a property called greeting, and the value of this property is a function.
+    console.log("Hello JS User!");
+    }
+
+JsUser.greeting()   // calling the function and we DO NOT USE console.log(JSUser.greeting()) because it will print and then also return undefined. Also the function itself is already printing the greeting, so we just need to call it instead of printing it again.
+
+
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS User, ${this.name}!`)     // this: Refers to the object (JsUser) that owns the method.
+}
+JsUser.greetingTwo()
+
