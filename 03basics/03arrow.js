cc69c7cbb5e2c3b25f09a5ea832ca9347f269ef3
Swarnjeet <148362854+swarn6402@users.noreply.github.com>
2024-09-22 12:05:30 +0000

    Arrow functions in depth and with notes

--- 03basics/03arrow.js
+++ 03basics/03arrow.js
@@ -17,3 +17,30 @@ user.greeting()
 console.log(this);    // gives {} empty object because there is no context. but its a different case in browser console for javascript.
 
 
+// Arrow function in JS:
+
+/*
+const coffee = function() {
+    let username = "Swarn"    
+    console.log(this.username)   --> gives undefined because this is a global keyword and we attach it to a local variable username here.
+    }
+
+coffee()
+*/
+
+const addNos = (num1, num2) => {
+    return (num1 + num2)
+}
+ 
+console.log(addNos(5, 25));
+
+
+// Implicit return:    IMP for React
+
+const addTwo = (no1, no2) => (no1 + no2)    // return is not written, instead just paranthesis () is used.
+console.log(addTwo(5, 10));
+
+// When we wrap the function in {} curly bracaes, then the "return" keyword is used. 
+// When we use the parenthesis (), then the return keyword is not used.
+
+/* const addTwo = (no1, no2) => ({username : Swarn})  --> Paranthesis is used to return an object as well, otherwiswe it will return undefined. */  
