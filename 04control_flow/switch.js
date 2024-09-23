72333f55c94cd9a48cac1ecb0ce8f9950082da87
Swarnjeet <148362854+swarn6402@users.noreply.github.com>
2024-09-23 11:40:42 +0000

    Switch Case with numbers and strings

---
+++ 04control_flow/switch.js
@@ -0,0 +1,47 @@
+// Switch case in JS
+
+const month = 4
+
+switch (month) {
+    case 1:
+        console.log("It is January");
+        break;
+    case 2:
+        console.log("It is February");
+        break;
+    case 3:
+        console.log("It is March");
+        break;
+    case 4:
+        console.log("It is April");
+        break;
+
+    default:
+        break;
+}
+// Prints --> It is April
+
+
+// for string keys and values:
+
+const Day = "Monday"
+
+switch (Day) {
+    case "Monday":
+        console.log("It is Monday");
+        break;
+    case "Tuesday":
+        console.log("It is Tuesday");
+        break;
+    case "Friday":
+        console.log("It is Friday");
+        break;
+    case "Sunday":
+        console.log("It is Sunday");
+        break;
+
+    default:
+        break;
+}
+
+// --> prints It is Monday
