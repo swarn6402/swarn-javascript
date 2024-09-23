cc9f97a27bd0a331af1b99aea711c4b455427c50
Swarnjeet <148362854+swarn6402@users.noreply.github.com>
2024-09-23 14:49:46 +0000

    truthy and falsy

---
+++ 04control_flow/truthyfalsy.js
@@ -0,0 +1,26 @@
+/* Falsy vales are the values that return false when used in boolean context especially in conditionals and loops.
+   Example pf falsy values: false, 0, -0, "", null, undefined, 0n(BigInt), NaN  */
+
+const UserEmail = false
+
+if(UserEmail) {
+    console.log("Got user email");
+}
+
+else {
+    console.log("Email not found");
+   
+}   //  Email not found
+
+/* Truthy values retun true in boolean context. All values except the falsy ones are truthy values*/
+
+const myEmail = true
+
+if(myEmail) {
+    console.log("Got user email");
+}
+
+else {
+    console.log("Email not found");
+   
+}   // Got user email
