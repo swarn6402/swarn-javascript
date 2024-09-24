bf896457c88baded549d3560fff7328a626f431f
Swarnjeet <148362854+swarn6402@users.noreply.github.com>
2024-09-24 18:24:56 +0000

    for..in

---
+++ 05iterations/for...in.js
@@ -0,0 +1,12 @@
+// for-in loops:
+
+const myObj = {
+    JS : "Javascript",
+    Py : "Python",
+    Cpp : "C++",
+    Rb : "Ruby on Rails"
+}
+
+for (const key in myObj) {
+   console.log(myObj[key]);    
+    }
